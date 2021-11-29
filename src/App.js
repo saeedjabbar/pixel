e88f1ef28b0b0203adcd794cebdd5e3b515e0fc2
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-11-29 01:18:02 -0500

    routes setup, todo: context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react"
 import Header from "./components/Header"
-import Cart from "./pages/Cart"
 import Photos from "./pages/Photos"
+import Cart from "./pages/Cart"
+import { Switch, Route } from "react-router-dom"
 
 function App() {    
     return (
         <div>
             <Header />
-            <h1>Home Page</h1>
+            <Switch>
+                <Route exact path="/">
+                    <Photos />
+                </Route>
+                <Route exact path="/cart">
+                    <Cart />
+                </Route>
+            </Switch>
         </div>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from "react"
-import { Link, Switch, Route } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 function Header() {
     return (
         <header>
-            
             <Link to="/"><h2>Pic Some</h2></Link>
             <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
         </header>
+
+
     )
 }
 
